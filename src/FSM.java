b0350c91038f766785079d0e15f3dc21f660127e
condon18 <condon18@up.edu>
2017-10-21 12:21:11 -0700

    adding src subdirectory

---
+++ src/FSM.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+import com.sun.corba.se.spi.orbutil.fsm.State;
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
+public class FSM {
+	// Create a 5-tuple
+	// define states 
+	ArrayList<Integer> states = new ArrayList<Integer>();
+	
+	// define initial state
+	Integer initialState;
+	// define accept state
+	ArrayList<Integer> acceptingStates = new ArrayList<Integer>();
+
+	// define alphabet
+	// define transition
+	
+    ArrayList fsm 
+    newnew ArrayList();
+    System.out.println("Initial size of al: " + al.size());
+
+    // add elements to the array list
+    al.add("C");
+    al.add("A");
+    al.add("E");
+    al.add("B");
+    al.add("D");
+    al.add("F");
+    al.add(1, "A2");
+    System.out.println("Size of al after additions: " + al.size());
+
+ )  // display the array list
+    System.out.println("Contents of al: " + al);
+
+    // Remove elements from the array list
+    al.remove("F");
+    al.remove(2);
+    System.out.println("Size of al after deletions: " + al.size());
+    System.out.println("Contents of al: " + al);
+ }
+}
