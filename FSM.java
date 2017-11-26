6cab5b0d83229fb2250bf579d0ed48699180dd73
Teresa Condon <teresamariecondon@gmail.com>
2017-11-25 21:22:17 -0800

    Update FSM.java

--- FSM.java
+++ FSM.java
@@ -6,6 +6,23 @@ Public Class FSM ()
       ArrayList fsm ≠
       newnew ArrayList();
       System.out.println("Initial size of al: " + al.size());
+//Convert transitions to simple 
+//regular expressions. 
+
+ // Add new and final state 
+// with appropriate ε-transitions.
+
+RemoveRemove all other 
+states, one state at a time reconstructing regular 
+expression paths through the state being removed. 
+For each pair of incoming and outgoing paths via thestate being removed (from x to y, labeled α and β, 
+respectively), add a transition from x to y with the 
+regular expression αγ*β (where γ is the regular 
+expression on the self-loop on the state being 
+removed, or ε). Union together the regular 
+expressions for parallel transitions. When only the 
+start and final state remain with one transition, that 
+regular expression is the answer
 
       // add elements to the array list
       al.add("C");
